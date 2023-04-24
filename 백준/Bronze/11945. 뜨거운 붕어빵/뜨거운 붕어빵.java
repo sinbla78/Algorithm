5cde0f1e61575da40969d9faaf21ef28d787ac02
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-04-24 18:57:59 +0900

    Update 뜨거운 붕어빵.java

--- 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
+++ 백준/Bronze/11945. 뜨거운 붕어빵/뜨거운 붕어빵.java
@@ -2,18 +2,20 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
 
-		for (int i = 0; i < x; i++) {
-			while (sc.hasNext()) {
-				StringBuilder sb = new StringBuilder(sc.next());
-				System.out.println(sb.reverse());
-			}
-		}
-		sc.close();
-	}
+        // x번 반복하면서 문자열을 입력받아 뒤집어 출력합니다.
+        for (int i = 0; i < x; i++) {
+            while (sc.hasNext()) {
+                StringBuilder sb = new StringBuilder(sc.next());
+                System.out.println(sb.reverse());
+            }
+        }
 
+        // Scanner 객체를 닫아줍니다.
+        sc.close();
+    }
 }
