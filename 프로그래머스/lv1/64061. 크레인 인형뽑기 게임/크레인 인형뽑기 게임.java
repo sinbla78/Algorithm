98260f288bafe643da285bc1ddf2c15c1cc78ccd
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-05-08 17:04:44 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 3.27 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/lv1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,39 +1,27 @@
 import java.util.Stack;
 
 class Solution {
-    public int solution(int[][] board, int[] moves) {
-        // 1. 연속된 인형들이 제거된 횟수.
+    public int solution(int[][] board, int[] moves) { //bord = 5*5 2차워 배열, 인형을 뽑는 위치를 담은 배열
         int answer = 0;
-        // 2. 인형을 담을 stack 바구니.
-        Stack<Integer> stack = new Stack<>();       
-        
-        // 3. moves의 크기는 크레인의 총 이동횟수.
-        for(int i =0;i<moves.length;i++){
-            // 3-1. j 인덱스를 이용해 보드의 행을 탐색. 열은 moves의 원소를 이용해 탐색.
-            for(int j=0;j<board.length;j++){
-                // 3-2. j행의 moves의 크레인위치 열에 해당하는 값에 인형이 존재한다면.
-                if(board[j][moves[i]-1] != 0 ) {                   
-                    // 3-3. stack이 비어있지 않고, 현재 스택의 최상단에 있는 인형과 크레인으로 뽑은 인형이 같다면
-                    if(!stack.empty() && stack.peek() == board[j][moves[i]-1]){
-                        // 인형들을 제거하는 횟수 증가.
-                        answer++;
-                        // 바구니에 있는 인형을 제거.
-                        stack.pop();
-                        // 크레인으로 뽑은 인형을 0으로 만들어 없애준다.
-                        board[j][moves[i]-1] = 0;
-                        break;
-                    // 3-4. 그 외의 경우는 인형을 바구니에 담은 후 0으로 없애준다.    
-                    }else{
-                        stack.push(board[j][moves[i]-1]);                      
-                        board[j][moves[i]-1] = 0;
+        Stack<Integer> stack = new Stack<>();
+        for (int move : moves) {
+            for (int j = 0; j < board.length; j++) {
+                if (board[j][move - 1] != 0) {
+                    if (stack.isEmpty()) {
+                        stack.push(board[j][move - 1]);
+                        board[j][move - 1] = 0;
                         break;
                     }
-                }                        
+                    if (board[j][move - 1] == stack.peek()) {
+                        stack.pop();
+                        answer += 2;
+                    } else
+                        stack.push(board[j][move - 1]);
+                    board[j][move - 1] = 0;
+                    break;
+                }
             }
         }
-        
-       
-        // 4. 없앤 인형의 수 = 인형을 없앤 수 * 2.
-        return answer*2;
+        return answer;
     }
 }
