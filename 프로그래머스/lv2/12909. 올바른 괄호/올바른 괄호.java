228a4020deec3710746ce3cc9712db69bcc27aac
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-05-08 16:47:24 +0900

    [level 2] Title: 올바른 괄호, Time: 9.54 ms, Memory: 52.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = false;
+        int count = 0;
+        for(int i = 0; i<s.length();i++){
+            if(s.charAt(i) == '('){
+                count++;
+            }
+            if(s.charAt(i) == ')'){
+                count--;
+            }
+            if(count < 0){
+                break;
+            }
+        }
+        if(count == 0){
+            answer = true;
+        }
+
+        return answer;
+    }
+}
