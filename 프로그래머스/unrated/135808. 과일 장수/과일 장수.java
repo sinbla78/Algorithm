3522ec36db45565dffe5e79be59674ac9632ac04
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-05-22 19:59:19 +0900

    [unrated] Title: 과일 장수, Time: 0.44 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -2,21 +2,17 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0; 
-        int boxNum = score.length / m;
+        int answer = 0;
+        
+        //오름차순으로 정렬
         Arrays.sort(score);
         
-        if(score.length % m == 0) {
-            for(int i = 0; i < boxNum; i++) {
-                answer += (score[i * m]) * m * 1;
-            }
-        } else {
-            int rest = score.length % m;
-            for(int i = 0; i < boxNum; i++) {
-                answer += (score[rest + (i * m)]) * m * 1;
-            }
+        // score 배열의 길이부터 시작하여 m보다 큰 값까지 m만큼 간격을 두며 반복
+        for (int i = score.length; i >= m; i -= m) {
+        // answer에 score[i - m]과 m을 곱한 값을 더함
+            answer += score[i - m] * m;
         }
-        
+
         return answer;
     }
 }
