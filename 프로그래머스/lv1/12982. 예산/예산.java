7fc372918b2a84b6794f57f3767b86dd9256e652
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-05-31 17:30:19 +0900

    [level 1] Title: 예산, Time: 0.36 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12982. 예산/예산.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+class Solution {
+    public int solution(int[] d, int budget) {
+        Arrays.sort(d);
+		int answer = 0;
+		
+		for (int i = 0; i < d.length; i++) {
+			if(budget>=d[i]) {
+				budget-=d[i];
+				answer++;
+			}
+            else break;
+		}
+        return answer;
+    }
+}
