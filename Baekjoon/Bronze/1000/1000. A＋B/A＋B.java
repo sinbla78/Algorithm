7b48ba79e6efbe1edfd5be110b0b0f7be6ee9658
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-06-25 17:38:54 +0900

    Update A＋B.java

--- Baekjoon/Bronze/1000/1000. A＋B/A＋B.java
+++ Baekjoon/Bronze/1000/1000. A＋B/A＋B.java
@@ -4,10 +4,10 @@ class Main {
   public static void main(String args[]) { 
   
 		Scanner in = new Scanner(System.in);
-		int A = in.nextInt();
-		int B = in.nextInt();
+		int A = in.nextInt();//입력
+		int B = in.nextInt();//입력
 		
-		System.out.println(A+B);
+		System.out.println(A+B);//출력
  
 		in.close();
   } 
