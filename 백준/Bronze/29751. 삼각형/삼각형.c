a473cfd4d71460dc5a7126d34540097044a70a69
양희범 <103175928+sinbla78@users.noreply.github.com>
2023-10-18 17:10:03 +0900

    [Bronze V] Title: 삼각형, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/29751. 삼각형/삼각형.c
+++ 백준/Bronze/29751. 삼각형/삼각형.c
@@ -3,7 +3,6 @@
 int main(void) {
     int a, b;
     scanf("%d %d", &a, &b);
-    printf("%.1f", 0.5* (float)a * (float)b);
+    printf("%.1f", 0.5*a*b);
     
-    return 0; // You should return an integer from the main function.
 }
